30b3e241569342f3de00d47a65d80ceecd6d99ef
victtor2 <vigarzon@itsqmet.edu.ec>
2025-04-28 13:02:38 -0500

    navegadores

---
+++ browsers/MainNavigator.tsx
@@ -0,0 +1,34 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import ScoreScreen from "../screens/ScoreScreen";
+import GameScreen from "../screens/GameScreen";
+
+const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
+
+function MyStack() {
+    return <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Drawer" component={MyDrawer} />
+    </Stack.Navigator>;
+  }
+
+  function MyDrawer() {
+    return <Drawer.Navigator initialRouteName="Score">
+                <Drawer.Screen name="Score" component={ScoreScreen} />
+                <Drawer.Screen name="Game" component={GameScreen} />
+                <Drawer.Screen name="Stack" component={MyStack} />
+            </Drawer.Navigator>;
+  }
+
+  export default function NavegadorPrincipal(){
+    return (
+        <NavigationContainer>
+            <MyStack />
+        </NavigationContainer>
+    )
+  }
